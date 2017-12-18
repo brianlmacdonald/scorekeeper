e17b7873071c08f4f5fb4dda8656357ad88b4130
Brian Macdonald <brian.lmacdonald@gmail.com>
2017-12-18 12:47:44 -0500

    tallies bonuses and checks for winner

--- app/components/ScoreKeeper/Game/ScoreScreen.js
+++ app/components/ScoreKeeper/Game/ScoreScreen.js
@@ -3,43 +3,71 @@ import { connect } from 'react-redux'
 import {RaisedButton} from 'material-ui'
 import {Link} from 'react-router'
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import { checkForWinner, checkForLosers } from './gameFunctions'
+import {resetScoresAction} from '../../../store'
 
 
-const ScoreScreen = ({ players }) => {
+const ScoreScreen = ({ players, handleReset }) => {
   console.log(players)
-    return (
-      <div>
-      <Table className='scoreTable'>
-        <TableHeader
-          displaySelectAll={false}
-          adjustForCheckbox={false}>
-          <TableRow>
-            <TableHeaderColumn>Name</TableHeaderColumn>
-            <TableHeaderColumn>Score</TableHeaderColumn>
-            <TableHeaderColumn>Wins</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-          <TableBody displayRowCheckbox={false}>
-          {players.map(player => {
-            return (<TableRow key={player.id}>
-              <TableRowColumn>{player.name}</TableRowColumn>
-              <TableRowColumn>{player.score}</TableRowColumn>
-              <TableRowColumn>{player.wins}</TableRowColumn>
-            </TableRow>)
-          })}
-        </TableBody>
-      </Table>
-        <RaisedButton
-          containerElement={<Link to="/addScores" />}
-          label="yaniv!"
-        />
-      </div>
-    )
+    checkForLosers(players)
+    const winner = checkForWinner(players)
+    if (winner) {
+      return (
+        <div>
+          <h1>{`${winner.name} wins!`}</h1>
+          <button
+            onClick={(evt) => {
+              evt.preventDefault()
+              handleReset()
+            }}
+          >
+          play again?
+          </button>
+        </div>
+      )
+
+    } else {
+      return (
+        <div>
+        <Table className='scoreTable'>
+          <TableHeader
+            displaySelectAll={false}
+            adjustForCheckbox={false}>
+            <TableRow>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Score</TableHeaderColumn>
+              <TableHeaderColumn>Wins</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+            {players.map(player => {
+              return (<TableRow key={player.id}>
+                <TableRowColumn>{player.name}</TableRowColumn>
+                <TableRowColumn>{player.score}</TableRowColumn>
+                <TableRowColumn>{player.wins}</TableRowColumn>
+              </TableRow>)
+            })}
+          </TableBody>
+        </Table>
+          <RaisedButton
+            containerElement={<Link to="/addScores" />}
+            label="yaniv!"
+          />
+        </div>
+      )
+    }
 }
 
-const mapStateToProps = (state) => ({
-  players: state.players
+const mapState = (state) => ({
+  players: state.players,
+  rules: state.rules
 })
 
-const ScoreScreenContainer = connect(mapStateToProps)(ScoreScreen)
+const mapDispatch = (dispatch) => ({
+  handleReset(){
+    dispatch(resetScoresAction())
+  }
+})
+
+const ScoreScreenContainer = connect(mapState, mapDispatch)(ScoreScreen)
 export default ScoreScreenContainer;
