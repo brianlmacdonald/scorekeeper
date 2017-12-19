9a6075bafa09adef4a35a563d70f05575b916e09
Brian Macdonald <brian.lmacdonald@gmail.com>
2017-12-19 15:05:31 -0500

    adds firebase for persisting data

--- app/store/reducers/index.js
+++ app/store/reducers/index.js
@@ -3,6 +3,7 @@ import {combineReducers} from 'redux'
 import game from './game'
 import rules from './rules'
 import players from './players'
+import user from './user'
 
 const RootReducer = combineReducers({game, rules, players})
 
