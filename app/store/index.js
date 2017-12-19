8bab36e971d5ad29da11f74ce7b0e57f81d87662
Brian Macdonald <brian.lmacdonald@gmail.com>
2017-12-19 15:27:08 -0500

    test

--- app/store/index.js
+++ app/store/index.js
@@ -3,21 +3,23 @@ import rootReducer from './reducers/'
 import thunkMiddleWare from 'redux-thunk'
 import firebase from 'APP/fire'
 const db = firebase.database()
-const user = firebase.auth().currentUser
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleWare))
 
 export default store;
 export * from './reducers'
 
-
-store.subscribe(function(){
-  localStorage.setItem('game', JSON.stringify(store.getState()))
-})
-
-store.subscribe(function(){
-  console.log(user, 'user in store')
-  db.ref('users/' + user.uid).set({
-    game: store.getState()
-  })
-})
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    store.subscribe(function () {
+      console.log(user)
+      db.ref('users/' + user.uid).set({
+        game: store.getState()
+      })
+    })
+  } else {
+    store.subscribe(function () {
+      localStorage.setItem('game', JSON.stringify(store.getState()))
+    })
+  }
+});
--- app/store/reducers/index.js
+++ app/store/reducers/index.js
@@ -3,7 +3,6 @@ import {combineReducers} from 'redux'
 import game from './game'
 import rules from './rules'
 import players from './players'
-import user from './user'
 
 const RootReducer = combineReducers({game, rules, players})
 
